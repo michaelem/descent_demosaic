49a76dddd71e47636d91702344d1e5ebdae1e57d
Michael Emhofer <michael@emhofer.net>
2021-02-24 22:11:19 +0100

    Start new gem

---
+++ lib/descent_demosaic.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "descent_demosaic/version"
+
+module DescentDemosaic
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/descent_demosaic/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module DescentDemosaic
+  VERSION = "0.1.0"
+end
