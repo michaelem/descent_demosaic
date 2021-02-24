f8b5b956fdb155c415fa46b81e2fecd55e9e134c
Michael Emhofer <michael@emhofer.net>
2021-02-24 22:54:40 +0100

    Read input image and write output image

--- lib/descent_demosaic.rb
+++ lib/descent_demosaic.rb
@@ -1,8 +1,22 @@
 # frozen_string_literal: true
 
+require "chunky_png"
+
 require_relative "descent_demosaic/version"
 
 module DescentDemosaic
   class Error < StandardError; end
-  # Your code goes here...
+
+  # The main class used to demosaic an image.
+  class Demosaicer
+    def initialize(source, destination)
+      @source = source
+      @destination = destination
+    end
+
+    def run
+      image = ChunkyPNG::Image.from_file(@source)
+      image.save(@destination)
+    end
+  end
 end
--- test/descent_demosaic_test.rb
+++ test/descent_demosaic_test.rb
@@ -3,11 +3,26 @@
 require "test_helper"
 
 class DescentDemosaicTest < Minitest::Test
+  def setup
+    @input1 = Pathname.new(
+      "test/resources/Mars_Perseverance_ESF_0004_0667289069_518ECM_N0010052EDLC00004_0010LUJ01.png"
+    )
+    @output = Pathname.new("tmp/test.png")
+    @output.delete if @output.exist?
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::DescentDemosaic::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_it_writes_an_output_file
+    demosaicer = DescentDemosaic::Demosaicer.new(@input1, @output)
+    demosaicer.run
+
+    assert File.exist?(@output)
+  end
+
+  def teardown
+    @output.delete if @output.exist?
   end
 end
